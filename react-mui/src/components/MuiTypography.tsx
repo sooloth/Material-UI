ef75697457514344f1792110b03d2040a6e51fc3
sooloth <pangnuar@gmail.com>
2024-01-16 13:32:50 +0700

    first

---
+++ react-mui/src/components/MuiTypography.tsx
@@ -0,0 +1,17 @@
+import {Typography} from '@mui/material'
+
+export const MuiTypography = () => {
+    return (
+        <div>
+            <Typography variant="h1">H1 Heading</Typography>
+            <Typography variant="h2">H1 Heading</Typography>
+            <Typography variant="h3">H1 Heading</Typography>
+            <Typography variant="h4">H1 Heading</Typography>
+            <Typography variant="h5">H1 Heading</Typography>
+            <Typography variant="h6">H1 Heading</Typography>
+
+            <Typography variant="subtitle1">subtitle 1</Typography>
+            <Typography variant="subtitle2">subtitle 2</Typography>
+        </div>
+    )
+}
